66f7334ebd2d0363d54e4b2dcf9f033ecf957685
jheniferrrrrr <jhenifer.leite.oliveira@escola.pr.gov.br>
2026-09-23 11:08:08 +0000

    alteração

---
+++ script.js
@@ -0,0 +1,22 @@
+function mostrarAlerta(){
+    alert(Bom dia Turma do 2ºAno Exatas!)
+}
+
+function pedirNome(){
+     let nome= prompt (Informe o seu Nome:")
+     alert (Bom dia, " + nome)
+}
+
+function somarNumeros (){
+    let primeirovalor= NUmber (promp("Informe o primeiro número:"))
+    let segundovalor= NUmber (promp("Informe o segundo número:"))
+    let soma= primeirovalor+ segundovalor
+
+    alert("a soma dos numeros é: " + soma)
+}
+
+function mudarformataçao(){
+     let resultado 
+}
+
+     
